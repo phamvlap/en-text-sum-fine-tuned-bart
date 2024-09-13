d8364cbbeb402cdc44404fd3b30de575f6e6f456
phamlap <lapb2105580@student.ctu.edu.vn>
2024-09-13 09:18:12 +0700

    fix: fix directory path

--- summarization/utils/mix.py
+++ summarization/utils/mix.py
@@ -20,19 +20,19 @@ def make_dir(dir_path: str) -> None:
 
 def make_dirs(config: dict, dir_names: list[str]) -> None:
     for dir_name in dir_names:
-        dir_path = join_path(base_dir=config["base_dir"], sub_path=config[dir_name])
+        dir_path = config[dir_name]
         Path(dir_path).mkdir(parents=True, exist_ok=True)
 
 
 def get_weights_file_path(config: dict, epoch: str) -> str:
     return join_path(
-        base_dir=config["base_dir"],
-        sub_path=f"{config['model_dir']}/{config['model_basename']}{epoch}.pt",
+        base_dir=config["model_dir"],
+        sub_path=f"{config['model_basename']}{epoch}.pt",
     )
 
 
 def get_list_weights_file_paths(config: dict) -> None | list[Path]:
-    model_dir = join_path(base_dir=config["base_dir"], sub_path=config["model_dir"])
+    model_dir = config["model_dir"]
     model_basename = config["model_basename"]
     weights_files = list(Path(model_dir).glob(pattern=f"{model_basename}*.pt"))
     if len(weights_files) == 0:
